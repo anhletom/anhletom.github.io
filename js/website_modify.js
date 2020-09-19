e10302ed56821cc24de9e395444f8a15f65a1bc1
Anh Le <maianh_tom@yahoo.com.vn>
2020-09-20 00:17:27 +0300

    Add newsletter link to navigation bar

--- js/website_modify.js
+++ js/website_modify.js
@@ -9,3 +9,14 @@ document.body.style.fontFamily = 'impact';
 document.querySelectorAll('#hot .item').forEach(function(item){ 
     item.style.margin= '10px 20px' 
     item.style.boxShadow = '2px 2px 10px'})
+
+    var newsletterEl = document.createElement('li');
+    newsletterEl.classList = 'nav-item dropdown menu-large';
+    var navLinkEl = document.createElement('a')
+    navLinkEl.classList = 'nav-link';
+    navLinkEl.innerText = 'Newsletter';
+    newsletterEl.appendChild(navLinkEl)
+    document.querySelectorAll('#navigation .navbar-nav.mr-auto')[0].appendChild(newsletterEl);
+
+    
+    

f8eca52cc6031ba9813c0214ccf11bca21b4c113
Anh Le <maianh_tom@yahoo.com.vn>
2020-09-19 23:46:25 +0300

    Add styling for #hot .item Elements

--- js/website_modify.js
+++ js/website_modify.js
@@ -5,3 +5,7 @@ document.getElementById('slogan').innerText = "WE HATE OUR CUSTOMERS";
 document.getElementById('all').style.backgroundColor = '#2f7884';
 
 document.body.style.fontFamily = 'impact';
+
+document.querySelectorAll('#hot .item').forEach(function(item){ 
+    item.style.margin= '10px 20px' 
+    item.style.boxShadow = '2px 2px 10px'})

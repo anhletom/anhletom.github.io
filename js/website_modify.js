db1c2dfd6564218d48fd82c9152067ad25ff496f
Anh Le <maianh_tom@yahoo.com.vn>
2020-09-19 23:20:24 +0300

    add background and font

--- js/website_modify.js
+++ js/website_modify.js
@@ -1,3 +1,7 @@
 console.log("Hello World");
 
-document.getElementById('slogan').innerText = "WE HATE OUR CUSTOMERS"
+document.getElementById('slogan').innerText = "WE HATE OUR CUSTOMERS";
+
+document.getElementById('all').style.backgroundColor = '#2f7884';
+
+document.body.style.fontFamily = 'impact';
